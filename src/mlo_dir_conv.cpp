d08f7860ae52a7ba3e16d73ffb49fe4549893969
ltqin <ltqin@hotmail.com>
2020-07-17 16:03:08 +0800

    test

--- src/mlo_dir_conv.cpp
+++ src/mlo_dir_conv.cpp
@@ -157,8 +157,8 @@ static auto GetImplicitGemmWrWSolvers()
     return miopen::solver::SolverContainer<//miopen::solver::ConvHipImplicitGemmV4R4GenXdlopsWrWFp32,
                                           //miopen::solver::ConvHipImplicitGemmV4R4GenWrWXdlops
                                           // miopen::solver::ConvHipImplicitGemmV4R1WrW,
-                                          // miopen::solver::ConvHipImplicitGemmV4R4WrW
-                                          miopen::solver::ConvHipImplicitGemmV4R4GenWrWXdlopsFp16
+                                           miopen::solver::ConvHipImplicitGemmV4R4WrW
+                                          // miopen::solver::ConvHipImplicitGemmV4R4GenWrWXdlopsFp16
                                           >{};
 }
 

cdbcbd04191938d7ccd9941616193bfc5736a72e
ltqin <ltqin@hotmail.com>
2020-07-27 15:58:04 +0800

    printf

--- src/kernels/composable_kernel/include/tensor_operation/gridwise_gemm_xdlops_fp16_bfp16.hpp
+++ src/kernels/composable_kernel/include/tensor_operation/gridwise_gemm_xdlops_fp16_bfp16.hpp
@@ -957,12 +957,12 @@ struct GridwiseBatchGemmXdlops_gkmkpack_gknkpack_gmn_v2
                 reinterpret_cast<const typename vector_type<ABFloat, KPack>::MemoryType*>(
                     p_b_block);
             blockwise_gemm.Run(p_a_block_vec, p_b_block_vec, p_c_thread);
-
+*/
             block_sync_lds();
 
             // store next data to LDS
             a_blockwise_copy.RunStoreThreadBuffer(p_a_thread_buffer, p_a_block);
-            b_blockwise_copy.RunStoreThreadBuffer(p_b_thread_buffer, p_b_block);*/
+            b_blockwise_copy.RunStoreThreadBuffer(p_b_thread_buffer, p_b_block);
         }
 
         // tail

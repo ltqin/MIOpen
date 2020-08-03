970309ccfae8841e76561b0c53a983f5b652c596
ltqin <ltqin@hotmail.com>
2020-08-03 18:08:26 +0800

    change order of A matirx

--- src/kernels/composable_kernel/include/tensor_operation/gridwise_gemm_xdlops_fp16_bfp16.hpp
+++ src/kernels/composable_kernel/include/tensor_operation/gridwise_gemm_xdlops_fp16_bfp16.hpp
@@ -991,7 +991,7 @@ struct GridwiseBatchGemmXdlops_gkmkpack_gknkpack_gmn_v2
             }
             
             b_blockwise_copy.RunStoreThreadBuffer(p_b_thread_buffer, p_b_block);
-            a_blockwise_copy.RunStoreThreadBuffer(p_a_thread_buffer, p_a_block);
+            //a_blockwise_copy.RunStoreThreadBuffer(p_a_thread_buffer, p_a_block);
             
         }
 

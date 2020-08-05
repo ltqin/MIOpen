d6183ad992b984fa99419da8ccdc03846a73ac2f
ltqin <ltqin@hotmail.com>
2020-08-05 10:21:01 +0800

    test

--- src/kernels/composable_kernel/include/kernel_algorithm/gridwise_convolution_backward_weights_implicit_gemm_v4r4_xdlops_nchw_kcyx_nkhw.hpp
+++ src/kernels/composable_kernel/include/kernel_algorithm/gridwise_convolution_backward_weights_implicit_gemm_v4r4_xdlops_nchw_kcyx_nkhw.hpp
@@ -170,7 +170,10 @@ struct GridwiseConvolutionBackwardWeightsImplicitGemm_v4r4_xdlops_nchw_kcyx_nkhw
 
         if(get_thread_local_1d_id() == 0 && get_block_1d_id() == 0)
         {
-            printf("\n p_wei_global:%p  p_in_global:%p p_out_global:%p",p_wei_global,p_in_global,p_out_global);
+            printf("\n p_wei_global:%p  p_in_global:%p p_out_global:%p",
+                   static_cast<void*>(p_wei_global),
+                   static_cast<const void*>(p_in_global),
+                   static_cast<const void*>(p_out_global));
             printf("\nGridwiseConvolutionBackwardWeightsImplicitGemm_v4r4_xdlops_nchw_kcyx_nkhw\nA matrix:gemmk = %d  gemmm = %d gemmkpack = %d \nB matrix: gemmk = %d  gemmn = %d gemmkpack = %d \nC matrix: gemmm = %d  gemmn = %d \n  ###################",
                      a_gemmk, a_gemmm,a_gemmkpack, b_gemmk, b_gemmn, b_gemmkpack, c_gemmm, c_gemmn);
         }

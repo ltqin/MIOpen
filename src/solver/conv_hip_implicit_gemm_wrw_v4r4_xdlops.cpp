d71cbda910d5477ee4cc6f2129357aeb5b20cba3
qinletao <ltqin@hotmail.com>
2020-08-13 12:15:54 +0800

    tunable parameters

--- src/solver/conv_hip_implicit_gemm_wrw_v4r4_xdlops.cpp
+++ src/solver/conv_hip_implicit_gemm_wrw_v4r4_xdlops.cpp
@@ -668,6 +668,10 @@ bool PerformanceImplicitGemmWrwV4R4Xdlops::IsFastToBeUsedForTuning(
             if(ratio > 6.21)
                 return false;
         }
+        else{
+            if(ratio > 12.21)
+                return false;
+        }
     }
 
     // don't need too many waves per block

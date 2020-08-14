65d0b0c9ea99ff509541c91514c2183089b64c95
ltqin <ltqin@hotmail.com>
2020-08-14 16:10:00 +0800

    tunable big weights recover

--- src/solver/conv_hip_implicit_gemm_wrw_v4r4_xdlops.cpp
+++ src/solver/conv_hip_implicit_gemm_wrw_v4r4_xdlops.cpp
@@ -635,12 +635,7 @@ bool PerformanceImplicitGemmWrwV4R4Xdlops::IsFastToBeUsedForTuning(
         std::cout << "gemm_m: " << gemm_m << " gemm_n: " << gemm_n << " gridsize: " << grid_size << " ratio: " << ratio << " grid_size_max_blockwise_gemm: " << grid_size_max_blockwise_gemm <<  std::endl;
         std::cout << "GemmMPerBlock: " << GemmMPerBlock << " GemmNPerBlock: " << GemmNPerBlock << " GemmKPerBlock: " << GemmKPerBlock << " GemmKPack: " << GemmKPack << " GemmMPerWave: " << GemmMPerWave << " GemmNPerWave: " << GemmNPerWave << std::endl;
 
-        if(grid_size_max_blockwise_gemm > 900)
-        {
-            if(ratio < 2)
-                return false;
-        }
-        else if(grid_size_max_blockwise_gemm > 600)
+       if(grid_size_max_blockwise_gemm > 600)
         {
             if(ratio > 1.41)
                 return false;

c6d3e0cb57afd2cb626b5d99a435b331d91c2c8d
ltqin <ltqin@hotmail.com>
2020-08-14 16:11:52 +0800

    tunable big weights recover

--- src/solver/conv_hip_implicit_gemm_wrw_v4r4_xdlops.cpp
+++ src/solver/conv_hip_implicit_gemm_wrw_v4r4_xdlops.cpp
@@ -640,17 +640,17 @@ bool PerformanceImplicitGemmWrwV4R4Xdlops::IsFastToBeUsedForTuning(
             if(ratio > 1.41)
                 return false;
         }
-        else if(grid_size_max_blockwise_gemm > 480)
+        if(grid_size_max_blockwise_gemm > 480)
         {
             if(ratio > 1.81)
                 return false;
         }
-        else if(grid_size_max_blockwise_gemm > 360)
+        if(grid_size_max_blockwise_gemm > 360)
         {
             if(ratio > 2.21)
                 return false;
         }
-        else if(grid_size_max_blockwise_gemm > 240)
+        if(grid_size_max_blockwise_gemm > 240)
         {
             if(ratio > 3.21)
                 return false;

0b0036424a85202c7f7ee7df6e650f51b8ce4a16
ltqin <ltqin@hotmail.com>
2020-08-27 10:39:38 +0800

    add tunable

--- src/solver/conv_hip_implicit_gemm_wrw_v4r4_xdlops.cpp
+++ src/solver/conv_hip_implicit_gemm_wrw_v4r4_xdlops.cpp
@@ -95,15 +95,17 @@ bool PerformanceImplicitGemmWrwV4R4Xdlops::SetNextValue()
             break;
         if(!NextTwoPower<1, 8>(GemmKPack))
             break;
-        if(!NextTwoPower<4, 128>(GemmNPerWave))
+        if(!NextTwoPower<16, 128>(GemmNPerWave))
             break;
-        if(!NextTwoPower<4, 128>(GemmMPerWave))
+        if(!NextTwoPower<16, 128>(GemmMPerWave))
             break;
         if(!NextTwoPower<1, 8>(GemmKPerBlock))
             break;
-        if(!NextTwoPower<4, 256>(GemmNPerBlock))
+        if(!NextTwoPower<32, 256>(GemmNPerBlock))
             break;
-        if(!NextTwoPower<4, 256>(GemmMPerBlock))
+        if(!NextTwoPower<32, 256>(GemmMPerBlock))
+            break;
+        if(!NextTwoPower<1, 128>(GemmKBlocks))
             break;
         return false;
     } while(false);

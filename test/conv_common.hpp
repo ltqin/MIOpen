75bee1558d2e75a58aefd01c8f22ea32232036d8
ltqin <ltqin@hotmail.com>
2020-08-28 11:47:54 +0800

    mask test code in test_conv2d

--- test/conv_common.hpp
+++ test/conv_common.hpp
@@ -1898,10 +1898,13 @@ struct conv_driver : test_driver
 
                 if(do_backward_weights && !skip_backward_weights)
                 {
-                    //output.generate(gen_sign_value);
+                #if 1
+                    output.generate(gen_sign_value);
+                #else
                     auto gen_one = [=](auto... is) { return 1; };
                     output.generate(gen_one);
                     input.generate(gen_one);
+                #endif
                     verify(verify_backward_weights_conv<T>{
                         input, weights, output, filter, stats, 0, search, immed});
                 }
